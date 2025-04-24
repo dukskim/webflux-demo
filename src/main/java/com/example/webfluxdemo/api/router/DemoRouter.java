25150c31cbfbbcc27c5ee1bef8735950ce5dccfe
kdh <kdh>
2025-04-24 22:29:20 +0900

    * 라우터 설정 수정 (@Bean 어노테이션 추가)

--- src/main/java/com/example/webfluxdemo/api/router/DemoRouter.java
+++ src/main/java/com/example/webfluxdemo/api/router/DemoRouter.java
@@ -2,6 +2,7 @@ package com.example.webfluxdemo.api.router;
 
 import com.example.webfluxdemo.api.handler.DemoHandler;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
@@ -12,6 +13,7 @@ public class DemoRouter {
 
     private final DemoHandler demoHandler;
 
+    @Bean
     public RouterFunction route() {
         return RouterFunctions.route()
                 .GET("/api/v1/demo/data", demoHandler::demoData)
--- src/main/java/com/example/webfluxdemo/config/WebFluxConfig.java
+++ src/main/java/com/example/webfluxdemo/config/WebFluxConfig.java
@@ -5,9 +5,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.config.EnableWebFlux;
 
-@Configuration
-@EnableWebFlux
-@Slf4j
-@RequiredArgsConstructor
+//@Configuration
+//@EnableWebFlux
+//@Slf4j
+//@RequiredArgsConstructor
 public class WebFluxConfig {
 }
